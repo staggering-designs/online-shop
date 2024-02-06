9204e4ef6e9207a464165e2634e53f24acc216f8
staggering-designs <159008696+staggering-designs@users.noreply.github.com>
2024-02-06 16:28:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = 
+    this.innerHTML = '
         <head>
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,6 +12,7 @@ class MyHeader extends HTMLElement {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         </head>
+    '
       }
 }
 
