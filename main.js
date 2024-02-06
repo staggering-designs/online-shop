76bb055ceb70e3230f05f09303a804f8e972a506
staggering-designs <159008696+staggering-designs@users.noreply.github.com>
2024-02-06 16:37:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,55 @@ customElements.define('my-header', MyHeader)
 class MyFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        
+        <footer>
+          <div class="footer-nav">
+            <div class="container">
+              <ul class="footer-nav-list">
+                <li class="footer-nav-item">
+                  <h2 class="nav-title">Contact</h2>
+                </li>
+                <li class="footer-nav-item flex">
+                  <div class="icon-box">
+                    <ion-icon name="location-outline"></ion-icon>
+                  </div>
+                  <address class="content">
+                    Dunlop, Kilmarnock
+                  </address>
+                </li>
+                <li class="footer-nav-item flex">
+                  <div class="icon-box">
+                    <ion-icon name="mail-outline"></ion-icon>
+                  </div>
+                  <a href="mailto:staggeringdesigns.contact@gmail.com" class="footer-nav-link">staggeringdesigns.contact@gmail.com</a>
+                </li>
+              </ul>
+              <ul class="footer-nav-list">
+                <li class="footer-nav-item">
+                  <h2 class="nav-title">Follow Us</h2>
+                </li>
+                <li>
+                  <ul class="social-link">
+                    <li class="footer-nav-item">
+                      <a href="https://www.facebook.com/profile.php?id=100091538854325" class="footer-nav-link"><ion-icon name="logo-facebook"></ion-icon> </a>
+                    </li>
+                    
+                    <li class="footer-nav-item">
+                      <a href="https://www.instagram.com/staggeringdesigns/" class="footer-nav-link"> <ion-icon name="logo-instagram"></ion-icon> </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="footer-bottom">
+            <div class="container">
+              <p class="copyright">
+                Copyright &copy; <a href="#">Staggering Designs</a> all rights reserved.
+              </p>
+            </div>
+          </div>
+        </footer>
       `
     }
 }
