c32f5603268ada7dcd4a643a4ccec155b3867d7a
staggering-designs <159008696+staggering-designs@users.noreply.github.com>
2024-02-06 16:22:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = 
-
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Staggering Designs Catalog</title>
+        
+          <!--
+            - favicon
+          -->
+          <link rel="shortcut icon" href="./assets/images/logo/logo.jpg" type="image/x-icon">
+        
+          <!--
+            - custom css link
+          -->
+          <link rel="stylesheet" href="./assets/css/style-prefix.css">
+        
+          <!--
+            - google font link
+          -->
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet">
+        
+        </head>
       }
 }
 
